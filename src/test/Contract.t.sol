6135ddc8c7c0ad92ad32c87dc6f512f428774ff8
Gaussian-Process <hello.gaussian@gmail.com>
2022-04-18 12:27:08 -0700

    add sqrt

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -1,12 +1,47 @@
-// SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
 
+import {FixedPointMathLib} from "../FixedPointMathLib.sol";
+
 contract ContractTest is DSTest {
     function setUp() public {}
 
-    function testExample() public {
-        assertTrue(true);
+    function testFromSolmate() public {
+        assertEq(FixedPointMathLib.sqrt(0), 0);
+        assertEq(FixedPointMathLib.sqrt(1), 1);
+        assertEq(FixedPointMathLib.sqrt(2704), 52);
+        assertEq(FixedPointMathLib.sqrt(110889), 333);
+        assertEq(FixedPointMathLib.sqrt(32239684), 5678);
     }
+
+    function testFromSolmate(uint256 x) public {
+        uint256 root = FixedPointMathLib.sqrt(x);
+        uint256 next = root + 1;
+
+        // Ignore cases where next * next overflows.
+        unchecked {
+            if (next * next < next) return;
+        }
+
+        assertTrue(root * root <= x && next * next > x);
+    }
+
+    // check exhaustively for x < 256
+    function testSqrtLt256() public {
+        for (uint i = 0; i < 16; i++) {
+            uint maxJ = (i+1) * (i+1);
+            for (uint j = i * i; j < maxJ; j++) {
+                assertEq(FixedPointMathLib.sqrt(j), i);
+            }
+        }
+    }
+
+    function testMore() public {
+        require(FixedPointMathLib.sqrt(99) == 9);
+        require(FixedPointMathLib.sqrt(100) == 10);
+        require(FixedPointMathLib.sqrt(1<<254) == 1<<127);
+        require(FixedPointMathLib.sqrt(256<<240) == 1<<124);
+    }
+
 }
